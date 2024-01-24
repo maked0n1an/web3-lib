6cc7954718f8996ed9cc11e04df433a625e0a232
maked0n1an <amedik733@gmail.com>
2024-01-25 01:27:19 +0200

    Updated gas_price

--- async_eth_lib/models/transactions.py
+++ async_eth_lib/models/transactions.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from web3 import Web3
+
 from .token_amount import TokenAmount
 
 if TYPE_CHECKING:
@@ -11,7 +13,8 @@ class Transactions:
     def __init__(self, client: Client) -> None:
         self.client = client
 
-    async def gas_price(self) -> TokenAmount:
-        amount = await self.client.w3.eth.gas_price
+    @staticmethod
+    async def gas_price(w3: Web3) -> TokenAmount:
+        amount = await w3.eth.gas_price
 
         return TokenAmount(amount, wei=True)
