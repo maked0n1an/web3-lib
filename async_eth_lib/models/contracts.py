aa19d2cada5f8db7d96b913d3b62193b71e4e078
maked0n1an <amedik733@gmail.com>
2024-01-25 23:22:26 +0200

    Updated client

--- async_eth_lib/models/contracts.py
+++ async_eth_lib/models/contracts.py
@@ -2,13 +2,39 @@ from web3 import Web3
 from web3.contract import Contract, AsyncContract
 from eth_typing import ChecksumAddress
 
-from .default_abis import DefaultAbis
 from .account import Account
+from .default_abis import DefaultAbis
+from .token_amount import TokenAmount
 
 
 class Contracts:
     def __init__(self, account: Account):
         self.account = account
+
+    async def get_balance(
+        self,
+        token_address: str | ChecksumAddress | None = None,
+        address: str | ChecksumAddress | None = None,
+        decimals: int = 18
+    ) -> TokenAmount:
+        if not address:
+            address = self.account.address
+
+        address = Web3.to_checksum_address(address)
+
+        if token_address:
+            contract = await self.default_token(contract_address=token_address)
+
+            amount = await contract.functions.balanceOf(address).call()
+            decimals = await contract.functions.decimals().call()
+        else:
+            amount = await self.account.w3.eth.get_balance(account=address)
+
+        return TokenAmount(
+            amount=amount,
+            decimals=decimals,
+            wei=True
+        )
 
     async def default_token(self, contract_address: ChecksumAddress | str) -> Contract | AsyncContract:
         contract_address = Web3.to_checksum_address(contract_address)
