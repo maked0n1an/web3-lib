1b6701f208fb3b89391e7edadfdf92a9fdacb1ad
maked0n1an <amedik733@gmail.com>
2024-01-25 23:22:26 +0200

    Updated client

--- async_eth_lib/models/client.py
+++ async_eth_lib/models/client.py
@@ -28,28 +28,3 @@ class Client:
 
         self.contracts = Contracts(self.account)
         self.transactions = Transactions(self.account)
-
-    async def get_balance(
-        self,
-        token_address: str | ChecksumAddress | None = None,
-        address: str | ChecksumAddress | None = None,
-        decimals: int = 18
-    ) -> TokenAmount:
-        if not address:
-            address = self.account.address
-
-        address = Web3.to_checksum_address(address)
-
-        if token_address:
-            contract = await self.contracts.default_token(contract_address=token_address)
-
-            amount = await contract.functions.balanceOf(address).call()
-            decimals = await contract.functions.decimals().call()
-        else:
-            amount = await self.account.w3.eth.get_balance(account=address)
-
-        return TokenAmount(
-            amount=amount,
-            decimals=decimals,
-            wei=True
-        )
