bb4739368ab316ff6e2ec06e33d015e96563daea
maked0n1an <amedik733@gmail.com>
2024-01-27 01:03:06 +0200

    Data Models, helpers: updated.

--- async_eth_lib/utils/helpers.py
+++ async_eth_lib/utils/helpers.py
@@ -1,8 +1,20 @@
+import json
+import os
 from aiohttp import (
     ClientSession
 )
 
 import async_eth_lib.models.others.exceptions as exceptions
+
+def join_path(path: str | tuple | list) -> str:
+    if isinstance(path, str):
+        return path
+    return str(os.path.join(*path))
+
+
+def read_json(path: str | tuple | list, encoding: str | None = None) -> list | dict:
+    path = join_path(path)
+    return json.load(open(path, encoding=encoding))
 
 
 def text_between(text: str, begin: str = '', end: str = '') -> str:
