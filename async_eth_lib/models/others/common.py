ab9d996aa8e4f4d3f55092e14c743cc5c6a01d3d
maked0n1an <amedik733@gmail.com>
2024-01-27 00:12:58 +0200

    Common: reformated all common types.

--- async_eth_lib/models/others/common.py
+++ async_eth_lib/models/others/common.py
@@ -2,7 +2,8 @@ class AutoRepr:
     """Contains a __repr__ function that automatically builds the output of a class using all its variables."""
 
     def __repr__(self) -> str:
-        values = ('{}={!r}'.format(key, value) for key, value in vars(self).items())
+        values = ('{}={!r}'.format(key, value)
+                  for key, value in vars(self).items())
         return '{}({})'.format(self.__class__.__name__, ', '.join(values))
 
 
@@ -12,6 +13,5 @@ class Singleton:
 
     def __new__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__new__(cls)
-
+            cls._instances[cls] = super(Singleton, cls).__new__(cls,*args, **kwargs)
         return cls._instances[cls]
