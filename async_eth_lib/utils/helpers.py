c3338e91abfbf977725742c2b8678321f235b955
maked0n1an <amedik733@gmail.com>
2024-01-26 12:17:09 +0200

    Updated make_request()

--- async_eth_lib/utils/helpers.py
+++ async_eth_lib/utils/helpers.py
@@ -15,10 +15,10 @@ async def make_request(
         response = await session.request(method, url=url, **kwargs)
 
         status_code = response.status
+        json_response = await response.json()
+        
         if status_code <= 201:
-            json_response = await response.json()
-
             return json_response
 
         raise exceptions.HTTPException(
-            response=response, status_code=status_code)
+            response=json_response, status_code=status_code)
