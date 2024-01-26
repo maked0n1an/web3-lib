479e73fd1cca5a12bc90a961ae9f53c5e6f67ce8
maked0n1an <amedik733@gmail.com>
2024-01-26 11:59:27 +0200

    Contracts: added get_signature()

--- async_eth_lib/models/contracts.py
+++ async_eth_lib/models/contracts.py
@@ -5,11 +5,30 @@ from eth_typing import ChecksumAddress
 from .account_manager import AccountManager
 from .default_abis import DefaultAbis
 from .token_amount import TokenAmount
-
+from .types import types
+from async_eth_lib.utils.helpers import make_request
 
 class Contracts:
     def __init__(self, account_manager: AccountManager):
         self.account_manager = account_manager
+
+    @staticmethod
+    async def get_signature(hex_signature: str) -> list | None:
+        """
+        Find all matching signatures in the database of https://www.4byte.directory/.
+
+        :param str hex_signature: a signature hash.
+        :return list | None: matches found.
+        """
+        try:
+            url = f'https://www.4byte.directory/api/v1/signatures/?hex_signature={hex_signature}'
+            response = await make_request(method="GET", url=url)
+            results = response['results']
+            
+            return [m['text_signature'] for m in sorted(results, key=lambda result: result['created_at'])]
+        except:
+            return
+        
 
     async def get_balance(
         self,
@@ -18,7 +37,7 @@ class Contracts:
         decimals: int = 18
     ) -> TokenAmount:
         if not address:
-            address = self.account_manager.address
+            address = self.account_manager.account.address
 
         address = Web3.to_checksum_address(address)
 
@@ -35,6 +54,13 @@ class Contracts:
             decimals=decimals,
             wei=True
         )
+        
+    
+    # async def get_approved_amount(
+    #     self,
+    #     token: types.Contract,
+        
+    # )
 
     async def default_token(self, contract_address: ChecksumAddress | str) -> Contract | AsyncContract:
         contract_address = Web3.to_checksum_address(contract_address)
