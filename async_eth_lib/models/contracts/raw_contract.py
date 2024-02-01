3afd446fc42f92347578350f516970306d6be733
maked0n1an <amedik733@gmail.com>
2024-02-01 13:26:59 +0200

    Contract: major update

--- async_eth_lib/models/contracts/raw_contract.py
+++ async_eth_lib/models/contracts/raw_contract.py
@@ -11,20 +11,23 @@ class RawContract(AutoRepr):
     An instance of a raw contract.
 
     Attributes:
-        title str: a contract title.
+        title (str): a contract title.
         address (ChecksumAddress): a contract address.
-        abi list[dict[str, Any]] | str: an ABI of the contract.
+        abi (list[dict[str, Any]] | str): an ABI of the contract.
+        is_native_token (bool): is this contract native token of network (False)
 
     """
     title: str
     address: ChecksumAddress
     abi: list[dict[str, Any]]
+    is_native_token: bool
 
     def __init__(
         self,
         title: str,
         address: str,
-        abi: list[dict[str, Any]] | str
+        abi: list[dict[str, Any]] | str,
+        is_native_token: bool = False
     ) -> None:
         """
         Initialize the class.
@@ -38,3 +41,4 @@ class RawContract(AutoRepr):
         self.title = title
         self.address = Web3.to_checksum_address(address)
         self.abi = json.loads(abi) if isinstance(abi, str) else abi
+        self.is_native_token = is_native_token
