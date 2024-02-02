b4bf98dab68c5002491ca71abef37c389dfbcc61
maked0n1an <amedik733@gmail.com>
2024-02-02 11:57:12 +0200

    Currencies, Contracts, Stargate: update

--- tasks/stargate/stargate.py
+++ tasks/stargate/stargate.py
@@ -1,9 +1,7 @@
 from async_eth_lib.models.client import Client
-from async_eth_lib.models.networks.networks import Networks
 from async_eth_lib.models.swap.swap_info import SwapInfo
 from tasks.base_task import BaseTask
-from tasks.stargate.contract_data import ContractData
-
+from tasks.stargate.models import StargateNetworkInfo
 
 class Stargate(BaseTask):
     def __init__(
@@ -11,7 +9,7 @@ class Stargate(BaseTask):
         client: Client
     ) -> None:
         self.client = client
-        self.contract_data = ContractData
+        self.contract_data = StargateNetworkInfo()
         
     def swap(
         self,
