3509eb12a743e3020ffe911373fc7e7b5e85b19d
maked0n1an <amedik733@gmail.com>
2024-02-02 15:52:39 +0200

    WooFi: fixed

--- tasks/woofi/woofi_contracts.py
+++ tasks/woofi/woofi_contracts.py
@@ -7,22 +7,22 @@ from async_eth_lib.models.others.common import Singleton
 from async_eth_lib.utils.helpers import read_json
 
 
-class WoofiData(Singleton):
+class WoofiContracts(Singleton):
+    WOOFI_ROUTER_V2_ABI = read_json(
+        path=('data', 'abis', 'woofi', 'abi.json')
+    )
+    
     contracts_dict={
         'WooRouterV2': {
             Networks.Arbitrum.name: RawContract(
                 title='WooRouterV2_Arbitrum',
                 address='0x9aed3a8896a85fe9a8cac52c9b402d092b629a30',
-                abi=read_json(
-                    path=('data', 'abis', 'woofi', 'abi.json')
-                )
+                abi=WOOFI_ROUTER_V2_ABI
             ),
             Networks.Polygon.name: RawContract(
                 title='WooRouterV2_Polygon',
                 address='0x817Eb46D60762442Da3D931Ff51a30334CA39B74',
-                abi=read_json(
-                    path=('data', 'abis', 'woofi', 'abi.json')
-                )
+                abi=WOOFI_ROUTER_V2_ABI
             )
         }
     }
