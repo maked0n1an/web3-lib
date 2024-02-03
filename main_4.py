f8b49bac320adca38c795357e213340026d3b5d8
maked0n1an <amedik733@gmail.com>
2024-02-03 21:35:58 +0200

    main_4.py: updated

--- main_4.py
+++ main_4.py
@@ -1,24 +1,36 @@
 import asyncio
 
 from async_eth_lib.models.client import Client
+from async_eth_lib.models.others.constants import CurrencySymbol
 from async_eth_lib.models.swap.swap_info import SwapInfo
 from async_eth_lib.models.networks.networks import Networks
-from data.config import PRIVATE_KEY
+from tasks.stargate.stargate import Stargate
 from tasks.woofi.woofi import WooFi
+from data.config import PRIVATE_KEY
 
 
 async def main():
     client = Client(private_key=PRIVATE_KEY, network=Networks.Polygon)
     woofi = WooFi(client=client)
+    stargate = Stargate(client=client)
 
     swap_info = SwapInfo(
-        from_token='MATIC',
-        to_token='WBTC',
-        min_percent=40,
-        max_percent=50,        
+        from_token=CurrencySymbol.USDC,
+        to_token=CurrencySymbol.USDC_e,
+        amount=0.0034,
+        gas_price=1
     )
-
+    print('Started Woofi')
     res = await woofi.swap(swap_info)
+    print(res)
+    
+    swap_info = SwapInfo(
+        from_token=CurrencySymbol.USDC_e,
+        to_token=CurrencySymbol.USDT,
+        to_network='bsc'
+    )
+    print('Started Stargate')
+    res = await stargate.swap(swap_info)
     print(res)
 
 if __name__ == '__main__':
