e72f191bd484fd2ce6c1ed895abc473067883988
maked0n1an <amedik733@gmail.com>
2024-02-06 04:21:04 +0200

    SwapQuery: updated doc-strings.

--- async_eth_lib/models/swap/swap_query.py
+++ async_eth_lib/models/swap/swap_query.py
@@ -7,10 +7,10 @@ class SwapQuery:
     A class representing a swap query.
 
     Attributes:
-        from_token (RawContract): The contract of the token to swap from.
-        to_token (RawContract): The contract of the token to swap to.
-        from_amount (TokenAmount): The amount of the 'from' token.
-        min_to_amount (TokenAmount): The minimum amount of the 'to' token.
+        from_token (TokenContract): The contract of the token to swap from.
+        to_token (TokenContract): The contract of the token to swap to.
+        amount_from (TokenAmount): The amount of the 'from' token.
+        min_to_amount (TokenAmount | None): The minimum amount of the 'to' token.
 
     """
 
@@ -18,20 +18,20 @@ class SwapQuery:
         self,
         from_token: TokenContract,
         to_token: TokenContract,
-        from_amount: TokenAmount,
+        amount_from: TokenAmount,
         min_to_amount: TokenAmount | None = None
     ) -> None:
         """
         Initialize the SwapQuery class.
 
         Args:
-            from_token (RawContract): The contract of the token to swap from.
-            to_token (RawContract): The contract of the token to swap to.
-            from_amount (TokenAmount): The amount of the from token.
-            min_to_amount (TokenAmount): The minimum amount of the to token.
+            from_token (TokenContract): The contract of the token to swap from.
+            to_token (TokenContract): The contract of the token to swap to.
+            amount_from (TokenAmount): The amount of the from token.
+            min_to_amount (TokenAmount | None): The minimum amount of the to token.
 
         """
         self.from_token = from_token
         self.to_token = to_token
-        self.from_amount = from_amount
+        self.amount_from = amount_from
         self.min_to_amount = min_to_amount
