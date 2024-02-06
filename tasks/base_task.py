493f55f795171e3624b0f5fcae1142c21f24c29e
maked0n1an <amedik733@gmail.com>
2024-02-06 04:19:25 +0200

    BaseTask: all fixed

--- tasks/base_task.py
+++ tasks/base_task.py
@@ -4,10 +4,13 @@ import aiohttp
 from web3.types import TxParams
 
 from async_eth_lib.models.client import Client
-from async_eth_lib.models.contracts.raw_contract import RawContract
+from async_eth_lib.models.contracts.contracts import TokenContracts
+from async_eth_lib.models.contracts.raw_contract import TokenContract
 from async_eth_lib.models.others.constants import CurrencySymbol
+from async_eth_lib.models.others.params_types import ParamsTypes
 from async_eth_lib.models.others.token_amount import TokenAmount
 from async_eth_lib.models.swap.swap_info import SwapInfo
+from async_eth_lib.models.swap.swap_query import SwapQuery
 
 
 class BaseTask:
@@ -39,33 +42,66 @@ class BaseTask:
         second_arg: str,
         arg_type: str = 'args'
     ) -> str:
+        """
+        Validate inputs for a swap operation.
+
+        Args:
+            first_arg (str): The first argument.
+            second_arg (str): The second argument.
+            arg_type (str): The type of arguments (default is 'args').
+
+        Returns:
+            str: A message indicating the result of the validation.
+
+        Example:
+        ```python
+        result = validate_swap_inputs('ETH', 'USDT', arg_type='symbols')
+        print(result)
+        # Output: 'The symbols for swap() are different: ETH != USDT'
+        ```
+        """
         if first_arg.upper() == second_arg.upper():
             return f'The {arg_type} for swap() are equal: {first_arg} == {second_arg}'
 
     async def approve_interface(
         self,
-        token_address: str,
-        spender: str,
+        token_contract: ParamsTypes.TokenContract,
+        spender_address: ParamsTypes.Address,
         amount: TokenAmount | None = None,
         gas_price: float | None = None,
         gas_limit: int | None = None,
         is_approve_infinity: bool = True
     ) -> bool:
         """
-        Approve the specified spender to spend the specified amount of tokens on behalf of the account.
+        Approve spending of a specific amount by a spender on behalf of the owner.
 
         Args:
-            token_address (str): The token contract address.
-            spender (str): The address of the spender.
-            amount (TokenAmount | None): The amount of tokens to be approved. If None, the maximum available balance will be approved.
-            is_approve_infinity (bool): Whether to approve an infinite amount.
+            token_contract (ParamsTypes.TokenContract): The token contract instance.
+            spender_address (ParamsTypes.Address): The address of the spender.
+            amount (TokenAmount | None): The amount to approve (default is None).
+            gas_price (float | None): Gas price for the transaction (default is None).
+            gas_limit (int | None): Gas limit for the transaction (default is None).
+            is_approve_infinity (bool): Whether to approve an infinite amount (default is True).
 
         Returns:
             bool: True if the approval is successful, False otherwise.
 
+        Example:
+        ```python
+        approved = await approve_interface(
+            token_contract=my_token_contract,
+            spender_address='0x123abc...',
+            amount=TokenAmount(amount=100, decimals=18),
+            gas_price=20,
+            gas_limit=50000,
+            is_approve_infinity=False
+        )
+        print(approved)
+        # Output: True
+        ```
         """
         balance = await self.client.contract.get_balance(
-            token_address=token_address
+            token_contract=token_contract
         )
         if balance.Wei <= 0:
             return False
@@ -74,8 +110,8 @@ class BaseTask:
             amount = balance
 
         approved = await self.client.contract.get_approved_amount(
-            token=token_address,
-            spender=spender,
+            token_contract=token_contract,
+            spender_address=spender_address,
             owner=self.client.account_manager.account.address
         )
 
@@ -83,8 +119,8 @@ class BaseTask:
             return True
 
         tx = await self.client.contract.approve(
-            token=token_address,
-            spender=spender,
+            token_contract=token_contract,
+            spender_address=spender_address,
             amount=amount,
             gas_price=gas_price,
             gas_limit=gas_limit,
@@ -92,7 +128,7 @@ class BaseTask:
         )
         receipt = await tx.wait_for_tx_receipt(
             web3=self.client.account_manager.w3,
-            timeout=300
+            timeout=240
         )
         
         if receipt:
@@ -100,22 +136,35 @@ class BaseTask:
 
         return False
 
-    async def calculate_amount_from_for_swap(
+    async def compute_source_token_amount(
         self,
-        from_token: RawContract,
         swap_info: SwapInfo
-    ) -> TokenAmount:
+    ) -> SwapQuery:   
         """
-        Calculate the amount of tokens to be swapped based on the provided swap information.
+        Compute the source token amount for a given swap.
 
         Args:
-            from_token (RawContract): The source token for the swap.
             swap_info (SwapInfo): Information about the swap.
 
         Returns:
-            TokenAmount: The calculated amount of token to be swapped.
+            SwapQuery: The query for the swap.
 
-        """
+        Example:
+        ```python
+        swap_query = await compute_source_token_amount(swap_info=my_swap_info)
+        print(swap_query)
+        # Output: SwapQuery(from_token=..., to_token=..., from_amount=...)
+        ```
+        """     
+        from_token = TokenContracts.get_token(
+            network=self.client.account_manager.network.name,
+            token_ticker=swap_info.from_token
+        )
+        to_token = TokenContracts.get_token(
+            network=self.client.account_manager.network.name,
+            token_ticker=swap_info.to_token
+        )
+        
         if from_token.is_native_token:
             balance = await self.client.contract.get_balance()
 
@@ -128,14 +177,20 @@ class BaseTask:
                 )
         else:
             balance = await self.client.contract.get_balance(
-                token_address=from_token.address
+                token_contract=from_token
             )
             if not swap_info.amount:
                 token_amount = balance
-            else:
+            else:                
+                decimals = (
+                    from_token.decimals
+                    if from_token.decimals 
+                    else await self.client.contract.get_decimals(token_contract=from_token)
+                )
+                
                 token_amount = TokenAmount(
                     amount=swap_info.amount,
-                    decimals=await self.client.contract.get_decimals(contract_address=from_token.address)
+                    decimals=decimals
                 )
 
         if swap_info.amount_by_percent:
@@ -148,7 +203,61 @@ class BaseTask:
         if token_amount.Wei > balance.Wei:
             token_amount = balance
 
-        return token_amount
+        return SwapQuery(
+            from_token=from_token,
+            to_token=to_token,
+            amount_from=token_amount
+        )
+    
+    async def compute_min_destination_amount(
+        self,
+        swap_query: SwapQuery,
+        to_token_price: float,
+        slippage: int
+    ) -> SwapQuery:
+        """
+        Compute the minimum destination amount for a given swap.
+
+        Args:
+            swap_query (SwapQuery): The query for the swap.
+            to_token_price (float): The price of the destination token.
+            slippage (int): The slippage tolerance.
+
+        Returns:
+            SwapQuery: The updated query with the minimum destination amount.
+
+        Example:
+        ```python
+        min_destination_query = await compute_min_destination_amount(
+            swap_query=my_swap_query,
+            to_token_price=my_token_price,
+            slippage=1
+        )
+        print(min_destination_query)
+        # Output: SwapQuery(from_token=..., to_token=..., from_amount=..., min_to_amount=...)
+        ```
+        """
+        decimals = 0
+        if swap_query.to_token.is_native_token:
+            decimals = self.client.account_manager.network.decimals
+
+        else:
+            decimals = await self.client.contract.get_decimals(
+                token_contract=swap_query.to_token
+            )
+
+        min_to_amount = TokenAmount(
+            amount=to_token_price * (1 - slippage / 100),
+            decimals=decimals,
+            wei=True
+        )
+
+        return SwapQuery(
+            from_token=swap_query.from_token,
+            to_token=swap_query.to_token,
+            amount_from=swap_query.from_amount,
+            min_to_amount=min_to_amount
+        )
 
     async def get_binance_ticker_price(
         self,
