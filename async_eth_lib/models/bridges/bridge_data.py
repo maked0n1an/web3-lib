71d9ffa213fb4a1517247a005f3839d6db582a18
maked0n1an <amedik733@gmail.com>
2024-02-09 19:52:38 +0200

    Stargate: succesfull! Added swapETH and fixed LayerZero structure

--- async_eth_lib/models/bridges/bridge_data.py
+++ async_eth_lib/models/bridges/bridge_data.py
@@ -1,14 +1,11 @@
 from async_eth_lib.models.contracts.raw_contract import RawContract
-from async_eth_lib.models.contracts.contracts import TokenContracts
 
 
-class BridgeData:
+class TokenBridgeInfo:
     def __init__(
         self,
-        token_contract: TokenContracts,
         bridge_contract: RawContract,
         pool_id: int | None = None
     ) -> None:
-        self.token_contract = token_contract
         self.bridge_contract = bridge_contract
         self.pool_id = pool_id
--- tasks/layer_zero/_data/models.py
+++ tasks/layer_zero/_data/models.py
@@ -1,11 +1,11 @@
-from async_eth_lib.models.bridges.bridge_data import BridgeData
+from async_eth_lib.models.bridges.bridge_data import TokenBridgeInfo
 
 
 class LayerZeroNetworkData:
     def __init__(
         self,
         chain_id: int,
-        token_dict: dict[str, BridgeData],
+        bridge_dict: dict[str, TokenBridgeInfo],
     ) -> None:
         self.chain_id = chain_id
-        self.token_dict = token_dict
+        self.bridge_dict = bridge_dict
