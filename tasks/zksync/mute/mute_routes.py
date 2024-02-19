8f30fd15b644391cef604e99c6578d0b56359e1d
maked0n1an <amedik733@gmail.com>
2024-02-19 03:18:36 +0200

    Mute: added routes (not completed)

---
+++ tasks/zksync/mute/mute_routes.py
@@ -0,0 +1,93 @@
+import async_eth_lib.models.others.exceptions as exceptions
+
+from async_eth_lib.models.contracts.contracts import TokenContracts
+from async_eth_lib.models.others.constants import TokenSymbol
+from async_eth_lib.models.swap.route_info import RouteInfo
+
+
+class MuteRoutes:
+    routes = {
+        TokenSymbol.ETH : {
+            TokenSymbol.USDC: RouteInfo(
+                method_name='swapExactETHForTokensSupportingFeeOnTransferTokens',
+                addresses=[
+                    TokenContracts.ZKSYNC_WETH.address,
+                    TokenContracts.ZKSYNC_USDC.address
+                ],
+                bool_list=[False, False]
+            ),
+            TokenSymbol.USDT: RouteInfo(
+                method_name='swapExactETHForTokens',
+                addresses=[
+                    TokenContracts.ZKSYNC_WETH.address,
+                    TokenContracts.ZKSYNC_USDC.address,
+                    TokenContracts.ZKSYNC_USDT.address,
+                ],
+                bool_list=[True, True, False]
+            ),
+        },
+        TokenSymbol.USDC : {
+            TokenSymbol.ETH: RouteInfo(
+                method_name='swapExactTokensForETH',
+                addresses=[
+                    TokenContracts.ZKSYNC_USDC.address,
+                    TokenContracts.ZKSYNC_WETH.address
+                ],
+                bool_list=[True, False]
+            ),
+            TokenSymbol.USDT: RouteInfo(
+                method_name='swapExactTokensForTokens',
+                addresses=[
+                    TokenContracts.ZKSYNC_USDC.address,
+                    TokenContracts.ZKSYNC_USDT.address,
+                ],
+                bool_list=[True, False]
+            ),
+        },
+        TokenSymbol.USDT : {
+            TokenSymbol.USDC: RouteInfo(
+                method_name='swapExactTokensForTokens',
+                addresses=[
+                    TokenContracts.ZKSYNC_USDT.address,
+                    TokenContracts.ZKSYNC_USDC.address,
+                ],
+                bool_list=[True, False]
+            ),
+            TokenSymbol.ETH: RouteInfo(
+                method_name='swapExactTokensForETH',
+                addresses=[
+                    TokenContracts.ZKSYNC_USDT.address,
+                    TokenContracts.ZKSYNC_USDC.address,
+                    TokenContracts.ZKSYNC_WETH.address,
+                ],
+                bool_list=[True, True, False]
+            )
+        },
+    }
+    
+    
+    @classmethod
+    def get_route_info(
+        cls,
+        first_token: str,
+        second_token: str
+    ) -> RouteInfo:
+        first_token = first_token.upper()
+        second_token = second_token.upper()
+        
+        if first_token not in cls.routes:
+            raise exceptions.RouteNotAdded(
+                f"The '{first_token}' token has not been "
+                f"added to {cls.__name__} routes dict"
+            )
+        
+        available_token_routes = cls.routes[first_token]
+        
+        if second_token not in available_token_routes:
+            raise exceptions.RouteNotAdded(
+                f"The '{second_token}' as second token has not been "
+                f"added to {first_token} routes dict"
+            )
+        
+        return available_token_routes[second_token]
+        
