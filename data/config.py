e75d124c8e711c9c18cc64efd6407c03b32321bc
maked0n1an <amedik733@gmail.com>
2024-03-01 00:53:56 +0200

    config: added proxies.

--- data/config.py
+++ data/config.py
@@ -15,3 +15,6 @@ ABIS_DIR = os.path.join(ROOT_DIR, 'data', 'abis')
 
 with open('private_keys.txt', 'r') as file:
     PRIVATE_KEYS = [row.strip() for row in file]
+    
+with open('proxies.txt', 'r') as file:
+    PROXIES = [row.strip() for row in file]
