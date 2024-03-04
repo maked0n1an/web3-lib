4df23bba34c431921d0d41262c6243edbd14add1
maked0n1an <amedik733@gmail.com>
2024-03-04 23:39:49 +0200

    Removed singleton

--- tasks/layer_zero/stargate/stargate_data.py
+++ tasks/layer_zero/stargate/stargate_data.py
@@ -1,13 +1,12 @@
 from async_eth_lib.models.bridges.bridge_data import TokenBridgeInfo
 from async_eth_lib.models.bridges.network_data import NetworkData
 from async_eth_lib.models.networks.networks import Networks
-from async_eth_lib.models.others.common import Singleton
 from async_eth_lib.models.others.constants import TokenSymbol
 from async_eth_lib.models.bridges.network_data_fetcher import NetworkDataFetcher
 from tasks.layer_zero.stargate.stargate_contracts import StargateContracts
 
 
-class StargateData(NetworkDataFetcher, metaclass=Singleton):
+class StargateData(NetworkDataFetcher):
     networks_data = {
         Networks.Arbitrum.name: NetworkData(
             chain_id=110,
